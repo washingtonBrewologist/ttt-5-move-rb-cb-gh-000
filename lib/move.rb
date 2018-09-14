eff35a7da23bc09ec0db2fd032876ab5d0e96934
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 02:09:33 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,6 @@ end
 # code your input_to_index and move method here!
 def input_to_index(input)
   "#{input}".to_i
-   input = index
 end
 board =[" "," "," "," "," "," "," "," "," ",]
 def move(board, index, value)
